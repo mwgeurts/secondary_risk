c14d3627fa3f74a9af5bb716974356460c768c53
Mark Geurts <mark.w.geurts@gmail.com>
2018-06-12 15:02:46 -0500

    Adjusted UI for Windows

--- SecondaryRiskCalculator.m
+++ SecondaryRiskCalculator.m
@@ -64,7 +64,7 @@ warning('off', 'all');
 handles.output = hObject;
 
 % Set version handle
-handles.version = '1.1.1';
+handles.version = '1.1.2';
 set(handles.version_text, 'String', ['Version ', handles.version]);
 
 % Determine path of current application
